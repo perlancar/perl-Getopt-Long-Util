ec8e9af109285a99f13665f4b22b87df40a80702
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-07-09 11:55:16 +0700

    Add function: parse_getopt_long_opt_spec() and make humanize_getopt_long_opt_spec() use that

--- lib/Getopt/Long/Util.pm
+++ lib/Getopt/Long/Util.pm
@@ -3,47 +3,108 @@ package Getopt::Long::Util;
 use 5.010001;
 use strict;
 use warnings;
+use experimental 'smartmatch';
 
+# DATE
 # VERSION
 
 require Exporter;
 our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw(
+                       parse_getopt_long_opt_spec
                        humanize_getopt_long_opt_spec
-                       gospec2human
                );
 
-# old name, kept for backward-compat
-sub gospec2human { goto &humanize_getopt_long_opt_spec }
+sub parse_getopt_long_opt_spec {
+    my $optspec = shift;
+    $optspec =~ qr/\A
+               (?:--)?
+               (?P<name>[A-Za-z0-9_-]+|\?)
+               (?P<aliases> (?: \| (?:[A-Za-z0-9_-]+|\?) )*)?
+               (?:
+                   (?P<is_neg>!) |
+                   (?P<is_inc>\+) |
+                   (?:
+                       =
+                       (?P<type>[siof])
+                       (?P<desttype>|[%@])?
+                       (?:
+                           \{
+                           (?: (?P<min_vals>\d+), )?
+                           (?P<max_vals>\d+)
+                           \}
+                       )?
+                   ) |
+                   (?:
+                       :
+                       (?P<opttype>[siof])
+                       (?P<desttype>|[%@])
+                   ) |
+                   (?:
+                       :
+                       (?P<optnum>\d+)
+                       (?P<desttype>|[%@])
+                   )
+                   (?:
+                       :
+                       (?P<optplus>\+)
+                       (?P<desttype>|[%@])
+                   )
+               )?
+               \z/x
+                   or return undef;
+    my %res = %+;
+
+    if ($res{aliases}) {
+        my @als;
+        for (split /\|/, $res{aliases}) {
+            next unless length;
+            next if $_ eq $res{name};
+            next if $_ ~~ @als;
+            push @als, $_;
+        }
+        $res{opts} = [$res{name}, @als];
+    } else {
+        $res{opts} = [$res{name}];
+    }
+    delete $res{name};
+    delete $res{aliases};
+
+    $res{normalized} = join(
+        "",
+        join("|", sort @{ $res{opts} }),
+        ($res{type} ? ("=", $res{type}, $res{desttype},
+                       (defined($res{max_vals}) ? (defined($res{min_vals}) ? "{$res{min_vals},$res{max_vals}}" : "{$res{max_vals}}") : ())) : ()),
+        ($res{opttype} ? (":", $res{opttype}, $res{desttype}) : ()),
+        (defined($res{optnum}) ? (":", $res{optnum}, $res{desttype}) : ()),
+        ($res{optplus} ? (":", $res{optplus}, $res{desttype}) : ()),
+    );
+    if (defined $res{max_vals}) {
+        $res{min_vals} //= $res{max_vals};
+    }
+    \%res;
+}
 
 sub humanize_getopt_long_opt_spec {
     my $optspec = shift;
 
-    $optspec =~ s/\A--?//;
+    my $parse = parse_getopt_long_opt_spec($optspec)
+        or die "Can't parse opt spec $optspec";
 
-    my $type = 'flag';
-    if ($optspec =~ s/!$//) {
-        $type = 'bool';
-    } elsif ($optspec =~ s/(=\w)$//) {
-        $type = $1;
-    } elsif ($optspec =~ s/\+$//) {
-        # also a flag, increment by one like --more --more --more
-    } elsif ($optspec !~ /[A-Za-z0-9?]\z/) {
-        die "Sorry, can't parse opt spec '$optspec' yet (probably invalid?)";
-    }
-
-    my $res = "";
-    for (split /\|/, $optspec) {
+    my $res = '';
+    my $i = 0;
+    for (@{ $parse->{opts} }) {
+        $i++;
         $res .= ", " if length($res);
-        s/^--?//;
-        if ($type eq 'bool') {
+        if ($parse->{is_neg}) {
             $res .= "--(no)$_";
         } else {
             if (length($_) > 1) {
-                $res .= "--$_" . ($type =~ /^=/ ? $type : "");
+                $res .= "--$_";
             } else {
-                $res .= "-$_" . ($type =~ /^=/ ? $type : "");
+                $res .= "-$_";
             }
+            $res .= "=$parse->{type}" if $i==1 && $parse->{type};
         }
     }
     $res;
@@ -51,16 +112,27 @@ sub humanize_getopt_long_opt_spec {
 
 #ABSTRACT: Utilities for Getopt::Long
 
-=for Pod::Coverage ^(gospec2human)$
-
 =head1 FUNCTIONS
 
-=head2 humanize_getopt_long_opt_spec($gospec) => STR
+=head2 parse_getopt_long_opt_spec($str) => hash
 
-Convert Getopt::Long option specification like C<help|h|?> or <--foo=s> or
+Parse Getopt::Long option specification. Will produce a hash with some keys:
+C<opts> (array of option names), C<type> (string, type name), C<desttype>
+(either '', or '@' or '%'), C<is_neg> (true for C<--opt!>), C<is_inc> (true for
+C<--opt+>), C<min_vals> (int, usually 0 or 1), C<max_vals> (int, usually 0 or 1
+except for option that requires multiple values), C<normalized> (string,
+normalized form of C<$str>: '--' prefix will be stripped, options will be
+sorted). Will return undef if it can't parse C<$str>. Examples:
+
+ $res = parse_getopt_long_opt_spec('help|h|?'); # {opts=>['?','h','help'], type=>undef, num_vals=>1}
+ $res = parse_getopt_long_opt_spec('--foo=s');  # {opts=>['foo'], type=>'s', num_vals=>1}
+
+=head2 humanize_getopt_long_opt_spec($str) => str
+
+Convert L<Getopt::Long> option specification like C<help|h|?> or <--foo=s> or
 C<debug!> into, respectively, C<--help, -h, -?> or C<--foo=s> or C<--(no)debug>.
-The output is suitable for including in help/usage text.
-
+Will die if can't parse C<$str>. The output is suitable for including in
+help/usage text.
 
 =head1 SEE ALSO
 
--- t/01-basics.t
+++ t/01-basics.t
@@ -5,15 +5,31 @@ use strict;
 use warnings;
 
 use Getopt::Long::Util qw(
+                             parse_getopt_long_opt_spec
                              humanize_getopt_long_opt_spec
                      );
 use Test::More 0.98;
 
+# TODO: more extensive tests
+
+subtest parse_getopt_long_opt_spec => sub {
+    is_deeply(
+        parse_getopt_long_opt_spec('help'),
+        {opts=>['help'], normalized=>'help'});
+    is_deeply(
+        parse_getopt_long_opt_spec('--help|h|?'),
+        {opts=>['help', 'h', '?'], normalized=>'?|h|help'});
+    is_deeply(
+        parse_getopt_long_opt_spec('name|alias=i'),
+        {opts=>['name','alias'], type=>'i', desttype=>'', normalized=>'alias|name=i'});
+};
+
 subtest humanize_getopt_long_opt_spec => sub {
     is(humanize_getopt_long_opt_spec('help|h|?'), '--help, -h, -?');
+    is(humanize_getopt_long_opt_spec('foo!'), '--(no)foo');
     is(humanize_getopt_long_opt_spec('foo=s'), '--foo=s');
     is(humanize_getopt_long_opt_spec('--foo=s'), '--foo=s');
-    is(humanize_getopt_long_opt_spec('foo|bar=s'), '--foo=s, --bar=s');
+    is(humanize_getopt_long_opt_spec('foo|bar=s'), '--foo=s, --bar');
 };
 
 DONE_TESTING:
