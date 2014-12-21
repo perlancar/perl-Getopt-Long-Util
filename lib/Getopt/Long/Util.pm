460538aa1dc96f23c88a3cb4adb30129c232afe3
perlancar (@pc-home) <perlancar@gmail.com>
2014-12-21 22:23:48 +0700

    Add function: detect_getopt_long_script()

--- lib/Getopt/Long/Util.pm
+++ lib/Getopt/Long/Util.pm
@@ -1,19 +1,22 @@
 package Getopt::Long::Util;
 
+# DATE
+# VERSION
+
 use 5.010001;
 use strict;
 use warnings;
 use experimental 'smartmatch';
-
-# DATE
-# VERSION
 
 require Exporter;
 our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw(
                        parse_getopt_long_opt_spec
                        humanize_getopt_long_opt_spec
+                       detect_getopt_long_script
                );
+
+our %SPEC;
 
 sub parse_getopt_long_opt_spec {
     my $optspec = shift;
@@ -101,6 +104,113 @@ sub humanize_getopt_long_opt_spec {
     $res;
 }
 
+$SPEC{detect_getopt_long_script} = {
+    v => 1.1,
+    summary => 'Detect whether a file is a Getopt::Long-based CLI script',
+    description => <<'_',
+
+The criteria are:
+
+* the file must exist and readable;
+
+* (optional, if `include_noexec` is false) file must have its executable mode
+  bit set;
+
+* content must start with a shebang C<#!>;
+
+* either: must be perl script (shebang line contains 'perl') and must contain
+  something like `use Getopt::Long`;
+
+_
+    args => {
+        filename => {
+            summary => 'Path to file to be checked',
+            schema => 'str*',
+            description => <<'_',
+
+Either `filename` or `string` must be specified.
+
+_
+        },
+        string => {
+            summary => 'Path to file to be checked',
+            schema => 'buf*',
+            description => <<'_',
+
+Either `file` or `string` must be specified.
+
+_
+        },
+        include_noexec => {
+            summary => 'Include scripts that do not have +x mode bit set',
+            schema  => 'bool*',
+            default => 1,
+        },
+    },
+};
+sub detect_getopt_long_script {
+    my %args = @_;
+
+    (defined($args{filename}) xor defined($args{string}))
+        or return [400, "Please specify either filename or string"];
+    my $include_noexec  = $args{include_noexec}  // 1;
+
+    my $yesno = 0;
+    my $reason = "";
+
+    my $str = $args{string};
+  DETECT:
+    {
+        if (defined $args{filename}) {
+            my $fn = $args{filename};
+            unless (-f $fn) {
+                $reason = "'$fn' is not a file";
+                last;
+            };
+            if (!$include_noexec && !(-x _)) {
+                $reason = "'$fn' is not an executable";
+                last;
+            }
+            my $fh;
+            unless (open $fh, "<", $fn) {
+                $reason = "Can't be read";
+                last;
+            }
+            # for efficiency, we read a bit only here
+            read $fh, $str, 2;
+            unless ($str eq '#!') {
+                $reason = "Does not start with a shebang (#!) sequence";
+                last;
+            }
+            my $shebang = <$fh>;
+            unless ($shebang =~ /perl/) {
+                $reason = "Does not have 'perl' in the shebang line";
+                last;
+            }
+            seek $fh, 0, 0;
+            {
+                local $/;
+                $str = <$fh>;
+            }
+        }
+        unless ($str =~ /\A#!/) {
+            $reason = "Does not start with a shebang (#!) sequence";
+            last;
+        }
+        unless ($str =~ /\A#!.*perl/) {
+            $reason = "Does not have 'perl' in the shebang line";
+            last;
+        }
+        if ($str =~ /^\s*(use|require)\s+Getopt::Long(\s|;)/m) {
+            $yesno = 1;
+            last DETECT;
+        }
+        $reason = "Can't find any statement requiring Getopt::Long module";
+    } # DETECT
+
+    [200, "OK", $yesno, {"func.reason"=>$reason}];
+}
+
 #ABSTRACT: Utilities for Getopt::Long
 
 =head1 FUNCTIONS
@@ -127,6 +237,9 @@ Convert L<Getopt::Long> option specification like C<help|h|?> or <--foo=s> or
 C<debug!> into, respectively, C<--help, -h, -?> or C<--foo=s> or C<--(no)debug>.
 Will die if can't parse C<$str>. The output is suitable for including in
 help/usage text.
+
+=head2 detect_getopt_long_script(%args) => envres
+
 
 =head1 SEE ALSO
 
